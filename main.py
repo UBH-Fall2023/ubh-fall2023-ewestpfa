ed9b3c13d5b83f51540a30ef98e54af89419e297
S-shing <s.shing.smith@gmail.com>
2023-11-05 01:46:48 -0500

    working implementations, and secrets :3

--- main.py
+++ main.py
@@ -2,26 +2,36 @@
 
 '''
 DISCLAIMER:
-        This project's GUI is based on the tkinter interface
+        This project's GUI is based on the tkinter and pillow interface
 
         We do not own this interface and are using it for non-commercial purposes
 '''
 
 import tkinter as tk
 import parseResponse as pr
+from PIL import Image, ImageTk
+
 window = tk.Tk()
 window.title("Tell A Joke")
-
+tempimg= Image.open("bgimage.png")
+BGIMAGE = ImageTk.PhotoImage(tempimg)
+BACKGROUND_COLOR="#FEF2FF"
+ACCENT_COLOR="#7eabca"
+DARK_ACCENT_COLOR="#3a6c8f"
+FONT_COLOR="#f8f8ff"
+FONT_FAMILY=""
+FONT_BOLD=""
 WIDTH = 500
 HEIGHT = 360
 window.geometry(str(WIDTH)+"x"+str(HEIGHT))
 window.resizable(False,False)
-chatframe = tk.Frame(window)
+chatframe = tk.Label(window, image=BGIMAGE, bg='white')
+chatframe.image = BGIMAGE
+chatframe.place(x=WIDTH/2, y=HEIGHT/2)
 scrollable = tk.Canvas(chatframe)
 BUTTON_HEIGHT = 1
 BUTTON_WIDTH = 20
 INPUT_WIDTH = 50
-BACKGROUND_COLOR="#FFFFFF"
 sent = ""
 
 
@@ -29,8 +39,9 @@ def send():
     global sent
     text_box.delete("all")
     sent = input.get() +"\n"
-    text_box.create_text(window.winfo_width()*.9,window.winfo_height()*.12,text="You: ", anchor=tk.E)
-    text_box.create_text(window.winfo_width()*.9,window.winfo_height()*.2,text=sent, width=(window.winfo_width())*2/3, anchor=tk.E)
+    text_box.create_image(10, 10, anchor="center", image=BGIMAGE)
+    text_box.create_text(window.winfo_width()*.9,window.winfo_height()*.12,text="You: ", anchor=tk.E, fill=FONT_COLOR)
+    text_box.create_text(window.winfo_width()*.9,window.winfo_height()*.14,text=sent, width=(window.winfo_width())*2/3,  anchor=tk.NE, fill=FONT_COLOR)
     text_box.pack(fill=tk.BOTH)
     window.after(500, respond)
     input.delete(0,"end")
@@ -39,28 +50,35 @@ def respond():
     global sent
     if(sent != ""):
         response = pr.getResponse(sent)
-    text_box.create_text(window.winfo_width() * .1, window.winfo_height() / 2.4, text="boyidiot's Neural Network: ", width=(window.winfo_width()) * 2 / 3, anchor=tk.W,fill="#4df0ff")
-    text_box.create_text(window.winfo_width() * .1, window.winfo_height() / 2, text=response, width=(window.winfo_width()) * 2 / 3, anchor=tk.W)
+    text_box.create_text(window.winfo_width() * .1, window.winfo_height() / 2.4, text="boyidiot's Neural Network: ",
+                         width=(window.winfo_width()) * 2 / 3, anchor=tk.W,fill=FONT_COLOR)
+    text_box.create_text(window.winfo_width() * .1, window.winfo_height() / 2.2, text=response,
+                         width=(window.winfo_width()) * 2 / 3, anchor=tk.NW,fill=FONT_COLOR)
+
     text_box.pack(fill=tk.BOTH)
 
 
-
 chatframe.pack()
+labelBorder = tk.Frame(bg=ACCENT_COLOR)
 inputLabel = tk.Label(text="↓ Talk to me and I'll tell you a joke! ↓")
+input = tk.Entry(master=labelBorder,width = INPUT_WIDTH,highlightthickness=2,highlightcolor=DARK_ACCENT_COLOR, bd=0)
 scrollable.pack()
-input = tk.Entry(width = INPUT_WIDTH)
+inputLabel.pack()
+labelBorder.pack()
+input.pack(fill="both", expand=True,padx=2,pady=2)
 
-inputLabel.pack()
-inputLabel.pack()
-input.pack(fill=tk.BOTH)
 enter = tk.Button(text="Send", width = BUTTON_WIDTH, height = BUTTON_HEIGHT, command = send)
 enter.pack()
 
 def hitenter(event):
     send()
+
 window.bind('<Return>', hitenter)
 
+
 text_box=tk.Canvas(master=scrollable, width=WIDTH)
-text_box.create_text(WIDTH/2,HEIGHT/2.5,text="Welcome to boyidiot's totally advanced AI chatbot that will tell you jokes based on what you tell it! Let's get started, hello!", anchor=tk.N, width=WIDTH*(2/3))
+text_box.create_image(10,10,anchor="center",image=BGIMAGE)
+
+text_box.create_text(WIDTH/2,HEIGHT/2.5,text="Welcome to boyidiot's totally advanced AI chatbot that will tell you jokes based on what you tell it! Let's get started, hello!", anchor=tk.N, width=WIDTH*(2/3),fill=FONT_COLOR)
 text_box.pack(fill=tk.BOTH)
 window.mainloop()
